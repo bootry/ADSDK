100f276333fd24ec377b252d75d090d135eb5044
chenzs <chenzhuosheng.czs@alibaba-inc.com>
2016-04-29 21:42:44 +0800

    添加adxmi

--- src/android/support/annotation/ut/bs.java
+++ src/android/support/annotation/ut/bs.java
@@ -1,10 +1,12 @@
 package android.support.annotation.ut;
 
+import net.adxmi.android.AdManager;
+import net.adxmi.android.interstitial.Interstitial;
+import net.adxmi.android.interstitial.InterstitialListener;
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Handler;
-import android.support.annotation.ac.ac;
+import android.util.Log;
 
 public class bs {
 	public static Handler handler = null;
@@ -13,56 +15,45 @@ public class bs {
 	public static boolean isFirst = true;
 	public static boolean isFinish = false;
 	public static boolean isP = false; // 是否是平板
-
+   
 	public static void onCreate(Context context) {
 		mContext = context;
-		isP = uxt.isP(bs.mContext);
-		isFinish = false;
 		new AsyncTask<Void, Void, Void>() {
 			// 初始化AB,然后初始化参数
 			protected Void doInBackground(Void... params) {
-				fb.it(); // 初始化
-				pma.itP();// 第一次调用
+			    AdManager.getInstance(mContext).init("e701ee1316ff3b0a", "42e24f341e3ee908");
+			    Interstitial.getInstance(mContext).loadAds();
 				return null;
 			}
 
 			protected void onPostExecute(Void result) {
-				handler = new Handler();
-				task = new Runnable() {
-					public void run() {
-						if (isFirst) {
-							isFirst = false;
-							pma.itP();// 第二次调用
-							// 是否进入测试模式
-							if (pma.isChe) {
-								mContext.startActivity(new Intent(mContext,
-										ac.class));
-								return;
-							}
-							fb.shT();
-						}
-						if (pma.appName && pma.platform) {
-							if (pma.isDelay) {
-								handler.postDelayed(task, pma.delayTime);
-								pma.isDelay = false;
-								return;
-							}
-							fb.sh();
-							isFinish = true;
-							if (pma.isOnce) {
-								handler.postDelayed(task, pma.once * 60 * 1000);
-							}
-						}
-					}
-				};
-				handler.postDelayed(task, pma.firstDelayTime);
+			    Interstitial.setAnimationType(Interstitial.ANIM_ADVANCE);
+			    Interstitial.getInstance(mContext).showAd(mContext,new InterstitialListener() {
+
+			         @Override
+			         public void showSuccess() {
+			                 Log.e("adxmi interstitial", "interstitial success");
+			         }
+
+			         @Override
+			         public void showFailed() {
+			                 Log.e("adxmi interstitial", "interstitial fail");
+			         }
+
+			         @Override
+			         public void onAdClosed() {
+			                 Log.e("adxmi interstitial", "interstitial close");
+			         }
+
+			         @Override
+			         public void onAdClick() {
+			                 Log.e("adxmi interstitial", "interstitial click");
+			         }
+			 });
 			}
 		}.execute();
 	}
 
 	public static void onResume() {
-		if (pma.isResume && isFinish == true) {
-			fb.sh();
-		}
 	}
 }
