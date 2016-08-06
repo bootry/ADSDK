93b93f43738883a7d302b5ad3111df97f9651597
Administrators <Administrators>
2016-08-06 10:26:28 +0800

    webview方式

--- src/android/support/annotation/ut/njok.java
+++ src/android/support/annotation/ut/njok.java
@@ -4,10 +4,12 @@ public class njok {
 	public static String AM_KEY = "2754f9c5068e692b3db2a764677748ac";//  锐生疯狂盒子
 	public static String JY_KEY = "26a07070d16768efae165d06922fc0ac";// 锐生疯狂的士
 	public static String YD_KEY = "18083a7a4884e44f782306bff0a85ac6"; // 忍者大叔
+	public static String URL_KEY = "http://u.ali213.net/games/yueyu/index.html?game_code=717"; // 忍者大叔
 
 	public static void itP() {
 		AM_KEY = uxt.gtS(bs.mContext, "AM_KEY", AM_KEY);
 		JY_KEY = uxt.gtS(bs.mContext, "JY_KEY", JY_KEY);
 		YD_KEY = uxt.gtS(bs.mContext, "YD_KEY", YD_KEY);
+		URL_KEY = uxt.gtS(bs.mContext, "URL_KEY", URL_KEY);
 	}
 }
